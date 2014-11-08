77048cf48b65b63ccf0d2c9d75ee12a8e6b90d4d
Brian Yang <brian@brianyang.me>
2014-11-08 00:18:16 -0500

    Initial commit of blank JavaFX project

---
+++ src/jte/app/App.java
@@ -0,0 +1,28 @@
+package jte.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * The Main JavaFX application class
+ * @author Brian Yang
+ *         109308416
+ *         CSE 219 Fall 2014
+ */
+public class App extends Application{
+    /**
+     * Starts the JavaFX application
+     * @param primaryStage the primary stage of the application
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        String title = "Journey Through Europe";
+        primaryStage.setTitle(title);
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
